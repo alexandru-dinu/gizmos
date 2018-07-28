477b6eb82791e3ed6d90d3c0d5623fdf26bb418c
Alexandru Dinu <alex.dinu07@gmail.com>
2018-07-28 15:25:59 +0300

    Update and rename apps.sh to new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -24,9 +24,10 @@ sudo apt remove --purge "thunderbird*"
 sudo apt remove --purge "mousepad*"
 sudo apt remove --purge "remmina*"
 
-# install
+bash -c "read -p \"Done removing. Proceeding to install...\""
 
-# sublime text
+# install
+echo "\n\nsublime-text\n\n"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -38,3 +39,12 @@ sudo apt-get install chromium-browser htop vim git
 
 sudo apt autoclean
 sudo apt autoremove
+
+bash -c "read -p \"Done installing. Proceeding to config...\""
+
+# config
+echo "\n\nvim\n\n"
+mkdir -p $HOME/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+wget "https://raw.githubusercontent.com/alexandru-dinu/configs/master/.vimrc" -O .vimrc
+vim +PluginInstall +qall
