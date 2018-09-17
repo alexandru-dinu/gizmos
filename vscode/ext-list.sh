3009bfb958f987968456f53f6ab9cc43c0776e66
Alexandru Dinu <alex.dinu07@gmail.com>
2018-09-17 17:43:41 +0300

    use Sunburst

--- vscode/ext-list.sh
+++ vscode/ext-list.sh
@@ -1,5 +1,5 @@
 code --install-extension geeebe.duplicate
-code --install-extension gerane.Theme-Wombat
+code --install-extension gerane.theme-sunburst
 code --install-extension justusadam.language-haskell
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
