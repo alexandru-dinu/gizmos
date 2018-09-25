fa372a6da370d5c7a42068a6a2fa29dbda039a53
Alexandru Dinu <alex.dinu07@gmail.com>
2018-09-25 14:14:04 +0300

    Update deploy_all.sh

--- deploy_all.sh
+++ deploy_all.sh
@@ -26,7 +26,7 @@ code --install-extension geeebe.duplicate
 code --install-extension gerane.theme-sunburst
 code --install-extension justusadam.language-haskell
 code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptoolscat 
+code --install-extension ms-vscode.cpptools
 echo "DONE"
 echo
 
