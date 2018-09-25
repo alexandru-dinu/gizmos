bd7f5447fb5e204d0ce5936c466cbc542dbbabd6
Alexandru Dinu <alex.dinu07@gmail.com>
2018-09-25 14:14:26 +0300

    Delete ext-list.sh

--- vscode/ext-list.sh
+++
@@ -1,5 +0,0 @@
-code --install-extension geeebe.duplicate
-code --install-extension gerane.theme-sunburst
-code --install-extension justusadam.language-haskell
-code --install-extension ms-python.python
-code --install-extension ms-vscode.cpptools
