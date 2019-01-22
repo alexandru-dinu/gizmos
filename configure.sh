029ab73b4f90e140df264fd5a016086ac29d26f4
alexandru-dinu <alex.dinu07@gmail.com>
2019-01-22 11:03:48 +0200

    Put vscode extension in array.

--- configure.sh
+++ configure.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 green=`tput setaf 2`
 reset=`tput sgr0`
+
+declare -a vscode_exts=(
+    "gerane.theme-sunburst"
+    "geeebe.duplicate"
+    "justusadam.language-haskell"
+    "ms-python.python"
+    "ms-vscode.cpptools"
+    "13xforever.language-x86-64-assembly"
+)
+
 
 config_zsh () {
     echo "${green}Configuring Oh-My-Zsh...${reset}"
@@ -70,12 +80,10 @@ config_vscode () {
     ln -s `realpath vscode/keybindings.json` $VSCODE_CFG_DIR/keybindings.json
     ln -s `realpath vscode/settings.json` $VSCODE_CFG_DIR/settings.json
 
-    code --install-extension gerane.theme-sunburst
-    code --install-extension geeebe.duplicate
-    code --install-extension justusadam.language-haskell
-    code --install-extension ms-python.python
-    code --install-extension ms-vscode.cpptools
-    code --install-extension 13xforever.language-x86-64-assembly
+    for ext in "${vscode_exts[@]}"
+    do
+        code --install-extension $ext
+    done
 
     echo "Done!"
 }
