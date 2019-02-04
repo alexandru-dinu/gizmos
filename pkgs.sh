fa20d46ed4377a1987235723168afc46571b489d
Alexandru Dinu <alex.dinu07@gmail.com>
2019-02-04 02:43:12 +0200

    Better organization and more gizmos!

--- pkgs.sh
+++ pkgs.sh
@@ -3,52 +3,77 @@
 # for app in /usr/share/applications/*.desktop; do echo "${app:24:-8}"; done
 # https://kkovacs.eu/cool-but-obscure-unix-tools
 
-declare -a pkgs=(
-    "totem*"
-    "rhythmbox*"
-    "brasero*"
-    "parole*"
-    "catfish*"
-    "ristretto*"
-    "gedit*"
-    "pidgin"
-    "xfburn"
-    "simple-scan"
-    "hplip"
-    "gnome-orca"
-    "evolution"
-    "fluid"
-    "gnome-mahjongg"
-    "gnome-mines"
-    "gnome-sudoku"
-    "thunderbird*"
-    "mousepad*"
-    "remmina*"
-    "libreoffice-gtk"
-    "xreader"
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+
+declare -a to_remove=(
+    "totem*" "rhythmbox*" "brasero*" "parole*" "catfish*"
+    "ristretto*" "gedit*" "pidgin" "xfburn" "simple-scan"
+    "hplip" "gnome-orca" "evolution" "fluid" "gnome-mahjongg"
+    "gnome-mines" "gnome-sudoku" "thunderbird*" "mousepad*" "remmina*"
+    "libreoffice-gtk" "xreader"
 )
 
-# remove
-for p in "${pkgs[@]}"
+declare -a essentials=(
+    "build-essential"
+    "ssh" "zsh" "vim" "git" "ghc" "geeqie" "feh" "terminator" "npm"
+    "chromium-browser" "vlc"
+)
+
+declare -a gizmos=(
+    "htop" "glances" "tree" "aria2" "tig" "cloc" "xclip" "mtr" "tmux" "dstat"
+    "powertop"
+    "silversearcher-ag"
+)
+
+declare -a npm_pkgs=(
+    "imgur-upload-cli"
+)
+
+# remove unneeded pkgs
+for p in "${to_remove[@]}"
 do
+    echo "${red}${p}${reset}"
     sudo apt remove --purge "$p"
 done
 
-# update
+
 sudo apt update
 sudo apt upgrade
 
-# install
-sudo apt install ssh zsh vim git terminator
-sudo apt install ghc build-essential
-sudo apt install chromium-browser vlc
-sudo apt install geeqie feh tree htop
-sudo apt install npm && sudo ln -sf /usr/bin/nodejs /usr/bin/node
+
+# install essential pkgs
+for p in "${essentials[@]}"
+do
+    echo "${green}${p}${reset}"
+    sudo apt install "$p"
+done
+
+
+# post-configs
+sudo ln -sf /usr/bin/nodejs /usr/bin/node
+
+
+# install npm pkgs
+for p in "${npm_pkgs[@]}"
+do
+    echo "${green}${p}${reset}"
+    (npm list -g | grep -q ${p}) || sudo npm install -g ${p}
+done
+
+# install gizmos
+for p in "${gizmos[@]}"
+do
+    echo "${green}${p}${reset}"
+    sudo apt install "$p"
+done
+
 
 # clean
 sudo apt autoclean
 sudo apt autoremove
 
-# install gizmos
-sudo npm install -g imgur-upload-cli
-
+echo -e "\n${green}Done!${reset}"
