9b283426dc7f1e4fab2d0559061090435c849269
Alexandru Dinu <alex.dinu07@gmail.com>
2019-03-18 21:57:12 +0200

    More default imports for IPython.

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -3,6 +3,10 @@ c = get_config()
 c.InteractiveShell.colors = "Linux"
 
 c.InteractiveShellApp.exec_lines = [
+    'import os',
+    'import sys',
+    'import time',
+
     'import torch',
     'import torch.nn as nn',
     'import torch.tensor as tensor',
@@ -11,4 +15,5 @@ c.InteractiveShellApp.exec_lines = [
 
     'import numpy as np',
     'import matplotlib.pyplot as plt',
+    'from tqdm.auto import tqdm',
 ]
