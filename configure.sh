19ebec34d97f213007139184900040494ab7678f
Alexandru Dinu <alex.dinu07@gmail.com>
2019-05-18 03:15:01 +0300

    Fix path.

--- configure.sh
+++ configure.sh
@@ -32,7 +32,7 @@ config_zsh () {
     cd ~/.oh-my-zsh && git add . && git commit -m "Custom." && cd -
 
     prompt "environment variables"
-    python setup_env.py
+    python ~/configs.git/setup_env.py
 
     prompt "Done"
 }
