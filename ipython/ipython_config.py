0eebb61c262d26908cd02835c00ac6cadc9155b9
Alexandru Dinu <alex.dinu07@gmail.com>
2019-06-26 20:27:44 +0300

    More IPython defaults.

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -7,13 +7,18 @@ c.InteractiveShellApp.exec_lines = [
     'import sys',
     'import time',
 
+    'import numpy as np',
+
     'import torch',
     'import torch.nn as nn',
     'import torch.tensor as tensor',
     'import torch.autograd as autograd',
     'from torch.autograd import Variable',
 
-    'import numpy as np',
+    'import seaborn as sns',
     'import matplotlib.pyplot as plt',
+
+    'import pickle',
     'from tqdm.auto import tqdm',
+    'from pprint import pprint',
 ]
