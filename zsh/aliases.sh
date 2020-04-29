7764953caf95cd8a1f9ca0ea999f4588e372a554
Alexandru Dinu <alex.dinu07@gmail.com>
2020-04-29 16:21:31 +0800

    [zsh] add tvim alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -11,9 +11,11 @@ alias notes="vim $HOME/workspace/notes.txt"
 alias ccat="ccat -G Plaintext=\"reset\" -G Decimal=\"teal\" -G Keyword=\"darkgreen\""
 alias dis="objdump -d -M intel"
 
-bgopen () { xdg-open "$1" &> /dev/null & }
-
 c () { cd "$1" && ls }
+
+bgopen () { xdg-open "$1" &> /dev/null & disown }
+
+tvim () { terminator -x zsh -c "vim $1" &> /dev/null & disown }
 
 # ssh-add-key
 sak () {
@@ -27,7 +29,7 @@ rs () { (redshift -x && ([ "$1" = "x" ] && return || redshift -O "$1" -g 0.9)) &
 to_pdf () { libreoffice --headless --convert-to pdf "$1" }
 
 # git
-gdm () { git diff origin/master "$1" }
+gdm  () { git diff origin/master "$1" }
 gdvm () { for f in `git ls-files -m`; do git diff origin/master $f; done }
 gdvc () { for f in `git ls-files -m`; do git diff origin/"$(git rev-parse --abbrev-ref HEAD)" $f; done }
 
@@ -41,11 +43,10 @@ set_display () {
 
 # directly download gdrive links
 gdrive_download () {
-  CONFIRM=$(
-  	wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies \
-  	--no-check-certificate "https://docs.google.com/uc?export=download&id=$1" \
-    -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p' \
-  )
-  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
-  rm -rf /tmp/cookies.txt
+    CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies \
+              --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" \
+              -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+    rm -rf /tmp/cookies.txt
 }
