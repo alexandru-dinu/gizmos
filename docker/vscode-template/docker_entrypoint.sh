658e08703700dde5dc28c58c79d5a1019d307c05
Alexandru Dinu <alex.dinu07@gmail.com>
2020-10-19 16:46:27 +0300

    [misc] remove docker vscode template

--- docker/vscode-template/docker_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ $# -eq 0 ]
-    then
-        code-server2.1692-vsc1.39.2-linux-x86_64/code-server --allow-http --no-auth --data-dir /data /code
-    else
-        exec "$@"
-fi
