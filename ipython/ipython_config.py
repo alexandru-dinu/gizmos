e962d7ae99096e2205d5e6a259aa04f75e7a7755
Alexandru Dinu <alex.dinu07@gmail.com>
2020-10-22 23:26:10 +0300

    [ipython] stop importing packages by default

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,32 +1,7 @@
 c = get_config()
 
-c.InteractiveShell.colors = "Linux"
-c.InteractiveShell.editor = "vim"
-c.TerminalIPythonApp.display_banner = False
+c.InteractiveShell.colors                              = "Linux"
+c.TerminalIPythonApp.display_banner                    = True
 c.TerminalInteractiveShell.highlight_matching_brackets = True
-c.TerminalInteractiveShell.term_title = True
-
-c.InteractiveShellApp.exec_lines = [
-    'import os',
-    'import sys',
-    'import time',
-    'import math',
-    'import re',
-
-    'from functools import reduce',
-    'import operator',
-    'import itertools',
-
-    'import numpy as np',
-
-    'import torch as T',
-    'import torch.nn.functional as F',
-    'import torch.nn as nn',
-
-    'import seaborn as sns',
-    'import matplotlib.pyplot as plt',
-
-    'import pickle',
-    'from tqdm.auto import tqdm',
-    'from pprint import pprint',
-]
+c.TerminalInteractiveShell.term_title                  = True
+c.InteractiveShellApp.exec_lines                       = []
