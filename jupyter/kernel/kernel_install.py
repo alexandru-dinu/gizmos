2acf80e5f0228dcfabeaa8d30b28ed003f12f6fe
Alexandru Dinu <alex.dinu07@gmail.com>
2020-10-28 22:07:13 +0200

    [jupyter] use pathlib for kernel installation script

--- jupyter/kernel/kernel_install.py
+++ jupyter/kernel/kernel_install.py
@@ -1,28 +1,31 @@
 import os
 import json
-import argparse
 import shutil
+import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--py',   type=str, required=True)
-parser.add_argument('--name', type=str, required=True)
-parser.add_argument('--dir',  type=str, required=True)
+parser.add_argument('--python_path',  type=str, required=True)
+parser.add_argument('--display_name', type=str, required=True)
+parser.add_argument('--kernel_dir',   type=str, required=True)
 args = parser.parse_args()
 
+home = Path(os.environ['HOME'])
+
+kernel_dir = home / 'anaconda3/share/jupyter/kernels' / args.kernel_dir
+os.makedirs(kernel_dir, exist_ok=False)
+
 kernel = json.dumps({
-    "argv"        : [args.py, "-m", "ipykernel_launcher", "-f", "{connection_file}"],
-    "display_name": args.name,
+    "argv"        : [args.python_path, "-m", "ipykernel_launcher", "-f", "{connection_file}"],
+    "display_name": args.display_name,
     "language"    : "python"
 })
 
-root_dir = f'{os.environ["HOME"]}/anaconda3/share/jupyter/kernels/{args.dir}'
-os.makedirs(root_dir, exist_ok=False)
+with open(kernel_dir / 'kernel.json', 'wt') as fp:
+    fp.write(kernel)
 
-with open(f'{root_dir}/kernel.json', 'wt') as f:
-    f.write(kernel)
-
-for f in ['logo-32x32.png', 'logo-64x64.png']:
-    shutil.copyfile(f, f'{root_dir}/{f}')
+for logo in ['logo-32x32.png', 'logo-64x64.png']:
+    shutil.copyfile(logo, kernel_dir / logo)
 
 print('Kernel installed.')
 print('Do not forget to run `pip install --upgrade ipykernel` in the new environment.')
