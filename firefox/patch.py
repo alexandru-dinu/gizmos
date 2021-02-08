83ee4a3fde5a896c1f0fee483c6e2f148d2551d4
Alexandru Dinu <alex.dinu07@gmail.com>
2021-02-08 22:37:49 +0200

    [misc] apply shfmt and black

--- firefox/patch.py
+++ firefox/patch.py
@@ -8,20 +8,20 @@ import shutil
 import configparser
 from pathlib import Path
 
-home = Path(os.environ['HOME'])
-firefox_dir = home / '.mozilla/firefox'
+home = Path(os.environ["HOME"])
+firefox_dir = home / ".mozilla/firefox"
 
 config = configparser.ConfigParser()
-config.read(firefox_dir / 'profiles.ini')
+config.read(firefox_dir / "profiles.ini")
 
-profile_dir = firefox_dir / config['Profile0']['Path']
-cfg_file    = profile_dir / 'prefs.js'
-patch       = './prefs.js.patch'
+profile_dir = firefox_dir / config["Profile0"]["Path"]
+cfg_file = profile_dir / "prefs.js"
+patch = "./prefs.js.patch"
 
 # userContent.css
-chrome_dir = profile_dir / 'chrome'
-os.system(f'mkdir -p {chrome_dir} && cp -f userContent.css {chrome_dir}')
-print('+ added chrome/userContent.css')
+chrome_dir = profile_dir / "chrome"
+os.system(f"mkdir -p {chrome_dir} && cp -f userContent.css {chrome_dir}")
+print("+ added chrome/userContent.css")
 
 # patching
 print(f"> patching {cfg_file}")
@@ -30,7 +30,7 @@ p_def = open(cfg_file).read()
 p_own = [x.strip().split(":") for x in open(patch).readlines()]
 
 for pk, pv in p_own:
-    r = f'user_pref({pk}, {pv});'
+    r = f"user_pref({pk}, {pv});"
 
     try:
         m = re.findall(f"user_pref\({pk}, .*\);", p_def)[0]
--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 
-c.InteractiveShell.colors                              = "Linux"
-c.TerminalIPythonApp.display_banner                    = True
+c.InteractiveShell.colors = "Linux"
+c.TerminalIPythonApp.display_banner = True
 c.TerminalInteractiveShell.highlight_matching_brackets = True
-c.TerminalInteractiveShell.term_title                  = True
-c.InteractiveShellApp.exec_lines                       = []
+c.TerminalInteractiveShell.term_title = True
+c.InteractiveShellApp.exec_lines = []
