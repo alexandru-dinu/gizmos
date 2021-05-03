268a6fbc950f33d5fec7a9cd0a008b7af48a18c2
Alexandru Dinu <alex.dinu07@gmail.com>
2021-05-03 15:28:07 +0300

    [emacs] add initial config

---
+++ emacs/init.el
@@ -0,0 +1,14 @@
+(setq inhibit-startup-screen t)
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+(show-paren-mode 1)
+(set-fringe-mode 10)
+
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 110)
+
+(load-theme 'tango-dark)
