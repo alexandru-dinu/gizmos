db7078e7a689624f2f36ef4817d1fdf9f94e5f28
Alexandru Dinu <alex.dinu07@gmail.com>
2021-07-06 10:13:27 +0300

    [zsh] add :e and :q aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,6 +4,9 @@ alias estab="ss -ta | grep -i estab | grep -v '127.0.0.1' | sort -k4"
 alias ta="tmux attach"
 alias td="tmux detach"
 
+alias :e="nvim"
+alias :q="exit"
+
 c      () { cd "$1" && ls }
 rs     () { (redshift -x && ([ "$1" = "x" ] && return || redshift -O "$1" -g 0.8)) &> /dev/null }
 bgopen () { xdg-open "$1" &> /dev/null & disown }
