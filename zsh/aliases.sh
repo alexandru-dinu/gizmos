5d9bde997d677065cb0866abe290880ab9d8a0be
Alexandru Dinu <alex.dinu07@gmail.com>
2021-08-24 15:28:05 +0300

    [*sh] add function for scratchpad

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -11,7 +11,17 @@ c      () { cd "$1" && ls }
 rs     () { (redshift -x && ([ "$1" = "x" ] && return || redshift -O "$1" -g 0.8)) &> /dev/null }
 bgopen () { xdg-open "$1" &> /dev/null & disown }
 
-# git
+# scratchpad
+sp () {
+    local scratchpad="${HOME}/.scratchpad"
+    local sep=$(perl -E 'say "=" x 32')
+    local startup_text="${sep} $(date +'%Y-%m-%d %H:%M:%S') ${sep}"
+    local startup_cmd="execute \"normal Go${startup_text}\<esc>o\""
+
+    ${EDITOR:-vim} -c ${startup_cmd} ${scratchpad}
+}
+
+# git diff vs current branch
 gdc () {
     local branch=$(git branch --show-current)
     git diff origin/$branch "$1"
