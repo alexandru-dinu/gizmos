c8ed9932b90c12628cc98aa0b27fc4ad65f0735f
Alexandru Dinu <alex.dinu07@gmail.com>
2021-11-05 21:58:41 +0200

    [ipython] magic: add several common imports

--- python/startup/magic_import_common.py
+++ python/startup/magic_import_common.py
@@ -2,37 +2,27 @@ from IPython.core.magic import register_line_magic
 
 
 # From https://stackoverflow.com/a/64791087
-def _global_import(
-    object_name: str, short_name: str = None, context_module_name: str = None
-):
-    """import from local function as global import
+def _global_import(obj: str, syn: str = None, ctx: str = None):
+    """[from `ctx`] import `obj` [as `syn`]"""
+    explain = [f"import {obj}"]
+    if syn:
+        explain.append(f"as {syn}")
+    if ctx:
+        explain.insert(0, f"from {ctx}")
+    print(" ".join(explain))
 
-    Args:
-        object_name: the object name want to import,
-                     could be module or function
-        short_name: the short name for the import
-        context_module_name: the context module name in the import
-
-    Examples:
-        import os -> _global_import("os")
-
-        import numpy as np -> _global_import("numpy", "np")
-
-        from collections import Counter ->
-            _global_import("Counter", None, "collections")
-
-        from google.cloud import storage ->
-            _global_import("storage", None, "google.cloud")
-    """
-    if not short_name:
-        short_name = object_name
-    if not context_module_name:
-        globals()[short_name] = __import__(object_name)
+    if not syn:
+        syn = obj
+    if not ctx:
+        globals()[syn] = __import__(obj)
     else:
-        context_module = __import__(context_module_name, fromlist=[object_name])
-        globals()[short_name] = getattr(context_module, object_name)
+        globals()[syn] = getattr(__import__(ctx, fromlist=[obj]), obj)
 
 
 @register_line_magic
 def magic_import_common(line):
+    _global_import("Path", None, "pathlib")
     _global_import("numpy", "np")
+    _global_import("torch", "T")
+    _global_import("torch.nn", "nn")
+    _global_import("pandas", "pd")
