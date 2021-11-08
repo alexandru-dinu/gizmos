c1b9e479163004f00729f1a933cb9999c66530ba
Alexandru Dinu <alex.dinu07@gmail.com>
2021-11-08 22:08:32 +0200

    [doom-emacs] revert to using just `.el` files for config
    it's too early for org-mode

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,3 +1,5 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
 (setq user-full-name "Alexandru Dinu"
       user-mail-address "alex.dinu07@gmail.com")
 
@@ -5,6 +7,7 @@
 
 (setq doom-font (font-spec :family "Iosevka Fixed SS09" :weight 'medium :size 16))
 (setq display-line-numbers-type 'relative)
+
 (setq doom-theme 'doom-one)
 
 (setq org-directory "~/workspace/pkb.git/org")
