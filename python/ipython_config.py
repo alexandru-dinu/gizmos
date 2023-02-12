6e7fb3c2a32f2b4c33274c02730232b637ac9a02
Alexandru Dinu <alex.dinu07@gmail.com>
2023-02-12 23:01:57 +0200

    [python] fix traceback highlight color
    Ref: https://stackoverflow.com/a/74681224

--- python/ipython_config.py
+++ python/ipython_config.py
@@ -1,3 +1,9 @@
+try:
+    from IPython.core import ultratb
+    ultratb.VerboseTB._tb_highlight = "bg:ansired"
+except Exception as e:
+    print(f"Error patching bg color for tracebacks: {e}")
+
 c = get_config()
 
 c.InteractiveShell.colors = "Linux"
