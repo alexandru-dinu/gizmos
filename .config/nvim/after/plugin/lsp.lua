aa0baea244e408c3ba55c157fafa74805e444764
Alexandru Dinu <alex.dinu07@gmail.com>
2023-06-14 00:48:10 +0300

    [nvim] pylsp: ignore certain errors

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -16,7 +16,17 @@ lsp.setup()
 -- language-specific configs
 local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-lspconfig.pylsp.setup{}
+lspconfig.pylsp.setup{
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    ignore = {'E501', 'E402'},
+                }
+            }
+        }
+    }
+}
 
 local cmp = require('cmp')
 cmp.setup({
