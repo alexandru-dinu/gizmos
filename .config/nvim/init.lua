4ad9638605a2bca1be3ce94102b8702827b7d19a
Alexandru Dinu <alex.dinu07@gmail.com>
2023-07-12 13:01:19 +0300

    [nvim] split below & right

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,8 @@ vim.opt.shortmess:append({ I = true })
 vim.opt.showmode = false
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
