56a881bd3d2182e19f24fa8f8dbe6666d5fd5392
Alexandru Dinu <alex.dinu07@gmail.com>
2023-08-03 23:02:54 +0300

    [nvim] disable lsp for now as it's too distracting

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,29 +4,7 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({buffer = bufnr})
 end)
 
-lsp.ensure_installed({
-    'lua_ls',
-    'rust_analyzer',
-    'pylsp',
-    'pyright',
-})
-
 lsp.setup()
-
--- language-specific configs
-local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-lspconfig.pylsp.setup{
-    settings = {
-        pylsp = {
-            plugins = {
-                pycodestyle = {
-                    ignore = {'E501', 'E402'},
-                }
-            }
-        }
-    }
-}
 
 local cmp = require('cmp')
 cmp.setup({
