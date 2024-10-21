be81faf6bb7df09e60074e0cb43bf5724ee4c13c
Alexandru Dinu <alex.dinu07@gmail.com>
2024-10-21 23:09:55 +0300

    [nvim] use proper function

--- .config/nvim/after/plugin/misc.lua
+++ .config/nvim/after/plugin/misc.lua
@@ -2,4 +2,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
 
 -- junegunn/vim-easy-align
-vim.api.nvim_set_keymap("x", "ga", "<Plug>(EasyAlign)", { silent = true, desc = "[G]o [A]lign" })
+vim.keymap.set("x", "ga", "<Plug>(EasyAlign)", { silent = true, desc = "[G]o [A]lign" })
