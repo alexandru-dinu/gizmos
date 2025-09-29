85e95dbd4b2f2baa08867a8450126450666e8e63
alexandru-dinu <alex.dinu07@gmail.com>
2025-09-29 23:34:41 +0300

    [py] update env

--- python/upgrade/upgrade.sh
+++ python/upgrade/upgrade.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-# also remove any dependencies that are no longer required
-conda env update --file environment.yaml --prune
-
-# remove index cache, lock files, unused cache packages, tarballs, and logfiles
+conda env update --file environment.yaml
 conda clean --all
-
-# remove all items from the cache
 pip cache purge
